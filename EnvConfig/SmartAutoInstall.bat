2256ecca084aee5fe7bbbf662a8d7c1ca16acd8f
fajiang <34428923+fajiang@users.noreply.github.com>
2018-01-09 20:18:27 -0700

    Update path to paramters

--- EnvConfig/SmartAutoInstall.bat
+++ EnvConfig/SmartAutoInstall.bat
@@ -5,36 +5,44 @@ CLS
 @ECHO OFF
 ECHO.
 
-::ECHO Start to install Git
-::start /wait %InstallPackages%\Git-2.15.1.2-64-bit.exe /SILENT /norestart
+goto start
+::Parameter examples. Path with blank space should be quoted with "" to avoid errors
+set PTFSrcPath = "\\psbldfs\dfs\enterprise\PsTestFrameWork\856\PTF856_04D"
+set PTFWrkPath = "C:\Program Files\PeopleSoft\PeopleSoft Test Framework"
+set PTSrcPath  = "\\psbldfs\dfs\build\pt\ptship\pt85604d\install_Windows.ora"
+set PTWrkPath  = "C:\SmartAutomation\pt"
+set InstallPkg = "C:\SmartAutomation\Software"
+
+:start
+::ECHO Start to install Git - Ignore for slaves?
+::start /wait %InstallPkg%\Git-2.15.1.2-64-bit.exe /SILENT /norestart
 
 ECHO Start to copy/update PTF folder......
-xcopy %PTFSrcPath%\*.* "C:\Program Files\PeopleSoft\PeopleSoft Test Framework" /s /h /d /c /y
-
+if not exist "%PTFWrkPath%" md "%PTFWrkPath%"
+xcopy "%PTFSrcPath%\*.*" "%PTFWrkPath%" /s /h /d /c /y
 
 ECHO Start to copy/update PeopleTools folder
-xcopy %PTSrcPath%\*.* "C:\SmartAutomation\pt" /s /h /d /c /y
-
+if not exist "%PTWrkPath%" md "%PTWrkPath%"
+xcopy "%PTSrcPath%\*.*" "%PTWrkPath%" /s /h /d /c /y
 
 ECHO Download from master and copy to/update local installation package folder
-xcopy "\\den00qhy.us.oracle.com\c$\SmartAutomation\Software\*.*"  %InstallPackages% /s /h /d /c /y
-
+if not exist "%InstallPkg%" md "%InstallPkg%"
+xcopy "\\den00qhy.us.oracle.com\c$\SmartAutomation\Software\*.*"  "%InstallPkg%" /s /h /d /c /y
 
 ECHO Start to install VC so that pside.exe can work......
 cd\
 cd SmartAutomation
-start /wait %InstallPackages%\vc_redist.x64.exe /q /norestart
+start /wait %InstallPkg%\vc_redist.x64.exe /q /norestart
 ECHO install VC x64 successfully......
-start /wait %InstallPackages%\vc_redist.x86.exe /q /norestart
+start /wait %InstallPkg%\vc_redist.x86.exe /q /norestart
 ECHO install VC x86 successfully......
-
 
 ::Check if python2.7 is already installed. Continue to install silently if not installed and exit if else.
 ::Default folder is C:\python27
 ECHO Start to install python2.7 x64......
 cd\
 cd SmartAutomation
-start /wait %InstallPackages%\python-2.7.14.amd64.msi /qn
+start /wait %InstallPkg%\python-2.7.14.amd64.msi /qn
 ECHO install python2.7 successfully......
 
 echo start to set python sys path....... 
@@ -46,22 +54,19 @@ wmic ENVIRONMENT where "name='path' and username='<system>'" set VariableValue="
 echo apply path......
 set path=%path%c:\python27;
 
-
 :run
 ::Check if setuptools already installed
 if exist C:\Python27\Scripts\easy_install*.exe (goto run2) else (goto run1)
 
 :run1
 ::install setuptools
-
 ::replace mimetypes.py to avoid coding error during setuptools
 ECHO replace mimetypes.py....
-replace %InstallPackages%\mimetypes.py "C:\python27\Lib"
+replace %InstallPkg%\mimetypes.py "C:\python27\Lib"
 if errorlevel 0 echo Successfully installed!
-
 ::install setuptools
 ECHO Start to install SETUPTOOLS......
-cd %InstallPackages%
+cd %InstallPkg%
 cd setuptools*
 python setup.py install
 if errorlevel 0 echo Successfully installed!
@@ -74,7 +79,7 @@ if exist C:\Python27\Scripts\pip*.exe (goto run4) else (goto run3)
 :run3
 ::install pip 
 ECHO Start to install PIP......
-cd %InstallPackages%
+cd %InstallPkg%
 cd pip*
 python setup.py install
 if errorlevel 0 echo Successfully installed!
