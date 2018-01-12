d4f2fd262c209dabfd1c5bcebbcc2f5bf5d4c779
fajiang <fangfang.jiang@oracle.com>
2018-01-12 04:39:20 -0700

    bug fix

--- EnvConfig/SmartAutoConfig.bat
+++ EnvConfig/SmartAutoConfig.bat
@@ -7,20 +7,20 @@ ECHO.
 
 goto start
 ::Parameter examples. Path with blank space should be quoted with "" to avoid errors
-set SARootPath = "C:\SmartAutomation"
-set ScriptPath = "C:\SmartAutomation\Scripts"
-set InstallPkg = "C:\SmartAutomation\InstallPkg"
-set PTFLogPath = "C:\SmartAutomation\PTF_Log"
-set JenJobPath = "C:\Temp\workspace\Env_GitDownload" 
+set SARootPath="C:\SmartAutomation"
+set ScriptPath="C:\SmartAutomation\Scripts"
+set InstallPkg="C:\SmartAutomation\InstallPkg"
+set PTFLogPath="C:\SmartAutomation\PTF_Log"
+set JenJobPath="C:\Temp\workspace\Env_GitDownload" 
 
 :start
-set ScriptPath = "%SARootPath%\Scripts"
-set InstallPkg = "%SARootPath%\InstallPkg"
-set PTFLogPath = "%SARootPath%\PTF_Log"
-set JenJobPath = "C:\Temp\workspace\Env_GitDownload" 
+set ScriptPath="%SARootPath%\Scripts"
+set InstallPkg="%SARootPath%\InstallPkg"
+set PTFLogPath="%SARootPath%\PTF_Log"
+set JenJobPath="C:\Temp\workspace\Env_GitDownload" 
 if not exist %SARootPath% md %SARootPath%
-if not exist %ScriptPath%  md %ScriptPath%
-if not exist %InstallPkg%  md %InstallPkg%
+if not exist %ScriptPath% md %ScriptPath%
+if not exist %InstallPkg% md %InstallPkg%
 xcopy %JenJobPath%\EnvConfig  %ScriptPath% /s /h /d /c /y
 xcopy %JenJobPath%\CI         %ScriptPath% /s /h /d /c /y
 xcopy \\den00qhy.us.oracle.com\C$\SmartAutomation\Scripts\SmartAnalyze.jar  %ScriptPath% /s /h /d /c /y
